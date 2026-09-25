06bd7e4479f7725adad26026b56e773614af2545
Sall <faisalwildanh99@gmail.com>
2026-09-25 16:25:04 +0700

    drivers: Add Re:Kernel support
    Add Re:Kernel support and binder/signal hooks for process freezing/unfreezing.

--- kernel/signal.c
+++ kernel/signal.c
@@ -55,8 +55,14 @@
 #include <asm/unistd.h>
 #include <asm/siginfo.h>
 #include <asm/cacheflush.h>
+#ifdef CONFIG_REKERNEL
+#include <uapi/asm/signal.h>
+#include <../drivers/rekernel/rekernel.h>
+#endif /* CONFIG_REKERNEL */
 #include "audit.h"	/* audit_signal_info() */
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/signal.h>
 /*
  * SLAB caches for signal bits.
  */
@@ -1269,7 +1275,12 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 {
 	unsigned long flags;
 	int ret = -ESRCH;
+#ifdef CONFIG_REKERNEL
+	if (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)
+		rekernel_report(SIGNAL, sig, task_tgid_nr(current), current, task_tgid_nr(p), p, false, NULL);
+#endif /* CONFIG_REKERNEL */
 
+	trace_android_vh_do_send_sig_info(sig, current, p);
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, type);
 		unlock_task_sighand(p, &flags);
