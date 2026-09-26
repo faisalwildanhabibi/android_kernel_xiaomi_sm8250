1038711f8c961e923118823b707a7eca0420e5cc
Sall <faisalwildanh99@gmail.com>
2026-09-26 13:46:40 +0700

    fs/proc/cmdline: Implement fail-safe zero-trace bootloader verification sanitizer

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,22 +3,52 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/string.h>
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 extern struct static_key_false susfs_is_fake_cmdline_or_bootconfig_buffer_set;
 extern void susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+extern char *susfs_fake_cmdline_or_bootconfig;
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 	if (static_branch_likely(&susfs_is_fake_cmdline_or_bootconfig_buffer_set)) {
-		susfs_spoof_cmdline_or_bootconfig(m);
-		seq_putc(m, '\n');
-		return 0;
+		/* Fail-safe: ignore template comment files starting with '#' */
+		if (susfs_fake_cmdline_or_bootconfig &&
+		    susfs_fake_cmdline_or_bootconfig[0] != '#' &&
+		    susfs_fake_cmdline_or_bootconfig[0] != '\0') {
+			susfs_spoof_cmdline_or_bootconfig(m);
+			seq_putc(m, '\n');
+			return 0;
+		}
 	}
 #endif
-	seq_puts(m, saved_command_line);
+	/* Hardened kernel fallback: sanitize bootloader unlocked indicators */
+	if (saved_command_line) {
+		const char *src = saved_command_line;
+		const char *p;
+		while (*src) {
+			if (!strncmp(src, "androidboot.verifiedbootstate=orange", 36)) {
+				seq_puts(m, "androidboot.verifiedbootstate=green");
+				src += 36;
+			} else if (!strncmp(src, "androidboot.flash.locked=0", 26)) {
+				seq_puts(m, "androidboot.flash.locked=1");
+				src += 26;
+			} else {
+				p = strpbrk(src, " \t\n");
+				if (p) {
+					seq_write(m, src, p - src);
+					seq_putc(m, *p);
+					src = p + 1;
+				} else {
+					seq_puts(m, src);
+					break;
+				}
+			}
+		}
+	}
 	seq_putc(m, '\n');
 	return 0;
 }
